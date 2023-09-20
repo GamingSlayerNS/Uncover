a17f1bfb692a16845425d4d2bde412819d2a8bd6
ssc190010 <ssc190010@utdallas.edu>
2023-09-20 10:54:37 -0500

    Adding alphabetizing logic and refactoring code structure

---
+++ src/AlphabeticShift.ts
@@ -0,0 +1,19 @@
+class Alphabetizer {
+    alphabetize(input: string): string[] {
+        let sentences = input.split(". ")
+
+        return sentences.sort(function (a, b) {
+            return a.toLowerCase().localeCompare(b.toLowerCase())
+        })
+    }
+}
+
+class CircularShift {
+    circularShift() {return null}
+}
+
+class LineStorage {
+    storeLine() {return null}
+}
+
+export default Alphabetizer
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import Input from './components/input';
-import LineStorage from './components/line-storage';
-import CircularShift from './components/circular-shift';
-import Alphabetizer from './components/alphabetizer';
-import Output from './components/output';
 import Sidebar from './components/sidebar';
+import Alphabetizer from './AlphabeticShift';
 
 function App() {
+    const [inputText, setInputText] = useState('')
+    const [outputText, setOutputText] = useState([''])
+
     return (
         <div className="bg-gray-600 ml-16 h-screen">
             <Sidebar />
             <header className="container m-auto">
                 <div className="flex flex-col justify-start gap-4 m-auto bg-gray-700 px-4 h-screen">
-
-                    <h1 className="text-white text-5xl font-bold tracking-wide">Unravel</h1>
+                    <h1 className="text-white text-5xl font-bold tracking-wide">Uncover</h1>
                     <form className="flex flex-row gap-16 bg-gray-600 rounded px-8 py-6">
-                        <input className="shadow-lg appearance-none border rounded w-full text-gray-500 pl-4" placeholder='Search:' />
-                        <button className="bg-secondary hover:bg-blue-500 text-white font-bold 
-                        py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Submit</button>
+                        <input
+                            className="shadow-lg appearance-none border rounded w-full text-gray-500 pl-4"
+                            placeholder='Search:'
+                            onChange={(e) => {setInputText(e.target.value)}}
+                        />
+                        <button
+                            type='button'
+                            className="bg-secondary hover:bg-blue-500 text-white font-bold
+                                py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            onClick={() => {
+                                const alphabetizer = new Alphabetizer()
+                                setOutputText(alphabetizer.alphabetize(inputText))
+                            }}
+                        >
+                            Submit
+                        </button>
                     </form>
-                    <Input text={"Hello"} onClick={() => {}} />
-                    <LineStorage text={"Hello"} onClick={() => {}} />
-                    <CircularShift text={"Hello"} onClick={() => {}} />
-                    <Alphabetizer text={"Hello"} onClick={() => {}} />
-                    <Output text={"Hello"} onClick={() => {}} />
+                    <div>
+                        {outputText}
+                    </div>
                 </div>
             </header>
         </div>
@@ -32,16 +41,3 @@ function App() {
 }
 
 export default App;
-
-
-// import './styles/App.css';
-
-// function App() {
-//   return (
-//     <h1 className="text-3xl font-bold underline text-red-600">
-//       Simple React Typescript Tailwind Sample
-//     </h1>
-//   );  
-// }
-
-// export default App;
