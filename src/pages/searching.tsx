869ea986989da400938a12cd322b0167fb9f9192
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-11-06 18:06:19 -0600

    search is working

--- src/pages/searching.tsx
+++ src/pages/searching.tsx
@@ -1,15 +1,14 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
+import { getDocs } from 'firebase/firestore';
 
 import { useState } from "react";
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import '../styles/search.css';
 
 export default function Search() {
     const firestore = firebase.firestore();
     const dataRef = firestore.collection('KWIC');
-    //const query = dataRef.limit(10) as any;
-    const query = dataRef.orderBy('KWIC_ID').startAt('google').endAt('google~') as any;
-    const [data] = useCollectionData(query, {idField: 'id'} as any);
 
     const [searchText, setSearchText] = useState('');
 
@@ -20,15 +19,46 @@ export default function Search() {
         }
     }
 
+    function renderSearch(doc: any) {
+        const searchList = document.querySelector('#search-list');
+
+        let li = document.createElement('a');
+        let name = document.createElement('h1');
+        let url = document.createElement('strong');
+        let desc = document.createElement('h1');
+
+        li.setAttribute('data-id', doc.id);
+        name.textContent = doc.data().name;
+        name.classList.add("text-white");
+        name.classList.add("text-xl");
+        name.classList.add("mt-4");
+
+        url.textContent = doc.data().url;
+        url.classList.add("text-lg");
+        url.classList.add("text-secondary");
+        desc.textContent = " | " + doc.data().KWIC_ID;
+        desc.classList.add("inline")
+        desc.classList.add("text-white");
+        desc.classList.add("text-lg");
+
+        li.appendChild(name);
+        li.appendChild(url);
+        li.appendChild(desc);
+
+        searchList?.appendChild(li);
+    }
+
     return (
         <>
-            <div className="flex flex-row items-center mx-auto gap-8">
+            <div className="flex flex-row flex-wrap items-center mx-auto gap-8">
                 <h1 className="text-white text-center text-5xl font-bold tracking-wide pb-4">Un<strong className="text-secondary">cover</strong></h1>
                 
                 <form className="flex flex-row bg-gray-600 rounded-full p-4 my-4">
                     <input
                         id='input'
-                        className="shadow-lg appearance-none border rounded-l-full w-[32rem] h-10 text-gray-500 pl-4"
+                        className="shadow-lg appearance-none border rounded-l-full h-10 
+                         w-full lg:w-[32rem]
+                        text-gray-500 pl-4"
                         placeholder='Browse:'
                         value={searchText}
                         onChange={(e) => {setSearchText(e.target.value)}}
@@ -39,7 +69,17 @@ export default function Search() {
                         type='button'
                         className="bg-secondary hover:bg-blue-500 text-white font-bold
                             w-32 py-2 px-4 rounded-r-full focus:outline-none focus:shadow-outline"
-                        // onClick={}
+                        onClick={async () => {
+                            const query = dataRef.orderBy('KWIC_ID').startAt(searchText).endAt(searchText + '~') as any;
+                            const searchList = document.querySelector('#search-list');
+                            searchList?.replaceChildren();
+
+                            await getDocs(query).then(snapshot => {
+                                snapshot.docs.forEach(doc => {
+                                    renderSearch(doc);
+                                })
+                            })
+                        }}
                     >
                         Search
                     </button>
@@ -48,16 +88,7 @@ export default function Search() {
 
             <Divider />
 
-            <div>
-                {data && data.map((data, i) => 
-                    <a key={i} href={`https://${data.url}`} target='_blank' rel='noreferrer'>
-                        <h1 className="text-white text-xl mt-4" key={`name${i}`}>{data.name}</h1>
-                        <h1 className="text-white text-lg" key={`url${i}`}>
-                            <strong className="text-secondary">{data.url}</strong> | {data.KWIC_ID}
-                        </h1>
-                    </a>
-                )}
-            </div>
+            <div id='search-list'></div>
         </>
     )
 };
