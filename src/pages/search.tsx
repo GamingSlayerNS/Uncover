3d006a7f8eaade06c0bdbe1b556ee82980930cde
Naxel Santiago <nas180011@cslinux1.utdallas.edu>
2023-11-28 13:29:43 -0600

    Fix search enter key

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -16,24 +16,33 @@ export default function Search() {
 
     const firestore = firebase.firestore();
     const dataRef = firestore.collection('KWIC');
-    const query = dataRef.orderBy('name').limit(1000) as any;
+    const query = dataRef.orderBy('KWIC_ID1').limit(1000) as any;
     const [data] = useCollectionData(query, {idField: 'id'} as any) as any;
 
     const settingRef = firestore.collection('settings');
     const settingQuery = settingRef.orderBy('mode').limit(1) as any;
     const [settings] = useCollectionData(settingQuery, {idField: 'id'} as any) as any;
 
-    function handleKeyPress(event: any){
+    function handleKeyPress(event: any, values: any){
         if (event.key === "Enter") {
             event.preventDefault();
-            document.getElementById("submitBtn")?.click();
+            handleChange(event, values);
+            // document.getElementById("submitBtn")?.click();
+
+            if (settings[0].mode === 'OR') {
+                navigate('searching-or', { state: { text: values.toLowerCase() } })
+            } else if (settings[0].mode === 'NOT') {
+                navigate('searching-not', { state: { text: values.toLowerCase() } })
+            } else {
+                navigate('searching', { state: { text: values.toLowerCase() } })
+            }
         }
     }
 
     const handleChange = (event: any, values: any) => {
         if (values) {
-            setSearchText(values.split(" ")[0].toLowerCase());
-            console.log(values.split(" ")[0].toLowerCase());
+            setSearchText(values.toLowerCase());
+            console.log(values.toLowerCase());
         }
     }
 
@@ -46,9 +55,10 @@ export default function Search() {
                     id='input'
                     className="shadow-lg appearance-none border rounded-full lg:rounded-r-none w-full lg:w-[32rem] h-14 text-gray-500 pl-4 bg-white"
                     freeSolo
-                    options={data && data.map((option: any) => option.name)}
+                    options={data && data.map((option: any) => option.KWIC_ID1)}
                     onInputChange={handleChange}
-                    onKeyDown={handleKeyPress}
+                    // onKeyDown={handleKeyPress}
+                    onChange={handleKeyPress}
                     renderInput={(params) => <TextField
                         className="h-14 rounded-full"
                         {...params}
@@ -66,6 +76,7 @@ export default function Search() {
                     className="bg-secondary hover:bg-blue-500 text-white font-bold
                         w-32 h-14 py-2 px-4 rounded-full lg:rounded-l-none focus:outline-none focus:shadow-outline"
                     onClick={() => {
+                        console.log(searchText);
                         if (settings[0].mode === 'OR') {
                             navigate('searching-or', { state: { text: searchText } })
                         } else if (settings[0].mode === 'NOT') {
